cd285d0deb07cfe0559e96255864dd0c89a867a4
Daniela <palamari.dana@gmail.com>
2025-08-31 16:36:54 +0300

    build: add Retrofit, Moshi, OkHttp, coroutines dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,24 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.0.0"
+    application
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    // HTTP client + API layer
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")            // Retrofit core (HTTP client abstraction)
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")     // JSON to Kotlin via Moshi
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")   // logs HTTP requests/responses
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
 }
