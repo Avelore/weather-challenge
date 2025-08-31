5a2d836ce743cf74d287f8230540d5fa6645fbe1
Daniela <palamari.dana@gmail.com>
2025-08-31 22:32:52 +0300

    added moshi-kotlin adapter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     // HTTP client + API layer
     implementation("com.squareup.retrofit2:retrofit:2.11.0")            // Retrofit core (HTTP client abstraction)
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")     // JSON to Kotlin via Moshi
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")            // Moshi’s Kotlin adapter dependency
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
